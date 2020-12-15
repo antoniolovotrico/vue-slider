b237d30e3d741fc84ebc59a806d1e648f359fe8d
antoniolovotrico <antonio.lovotrico@icloud.com>
2020-12-15 18:43:33 +0100

    start working on bonuses

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,22 @@
+let slideApp = new Vue ({
+    el: "#slideApp",
+    data: {
+        index: 0,
+        images: ["./assets/img/1.jpg","./assets/img/2.jpg","./assets/img/3.jpg","./assets/img/4.jpg"]
+    },
+    methods: {
+        nextArrow (){
+            this.index++;
+            if (this.index > this.images.length -1){
+                this.index = 0;
+            }
+        },
+        prevArrow (){
+            this.index--;
+            if (this.index < 0){
+                this.index = this.images.length -1;
+            }
+        }        
+    }
+    
+})
