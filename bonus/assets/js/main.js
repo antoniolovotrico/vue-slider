d6db985bf9f4810c28b2df6fcb80269e1da690cf
antoniolovotrico <antonio.lovotrico@icloud.com>
2020-12-15 19:52:13 +0100

    working on change img on click of relative circle

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -2,7 +2,7 @@ let slideApp = new Vue ({
     el: "#slideApp",
     data: {
         index: 0,
-        images: ["./assets/img/1.jpg","./assets/img/2.jpg","./assets/img/3.jpg","./assets/img/4.jpg"]
+        images: ["./assets/img/1.jpg","./assets/img/2.jpg","./assets/img/3.jpg","./assets/img/4.jpg"],
     },
     methods: {
         nextArrow (){
@@ -16,10 +16,16 @@ let slideApp = new Vue ({
             if (this.index < 0){
                 this.index = this.images.length -1;
             }
+        },
+        circleClick (){
+        
+                this.index = 3;
+            
+            
         }        
     },
     created () {
-        setInterval(this.nextArrow, 3000);
+        setInterval(this.nextArrow, 1000000);
     }
     
 })
